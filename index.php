39b4384a84ca9f133864007f1fe03ad06d4bfdc3
tidic <tidic84@gmail.com>
2025-02-19 15:52:50 +0100

    essai noraml

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
-<!DOCTYPE html>
 <?php
-    include './template/header.php';
+include './template/header.php';
 
-    $request = $_SERVER['REQUEST_URI'] ?? '/';
-        
-    switch($request) {
-        case '/':
-            require './template/home.php';
-            break;
-        case '/articles':
-            require './template/articles.php';
-            break;
-        case '/contact':
-            require './template/contact.php';
-            break;
-        default:
-            http_response_code(404);
-            require './template/404.php';
-            break;
-    }
+$page = $_GET['page'] ?? 'home';
 
+echo "Page demandée : " . htmlspecialchars($page);
 
+switch ($page) {
+    case 'home':
+        require './template/home.php';
+        break;
+    case 'articles':
+        require './template/articles.php';
+        break;
+    case 'contact':
+        require './template/contact.php';
+        break;
+    default:
+        http_response_code(404);
+        require './template/404.php';
+        break;
+}
 ?>
