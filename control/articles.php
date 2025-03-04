c2cbc092afcf7d51562c1b74d71020ddfed74a38
tidic <tidic84@gmail.com>
2025-03-04 15:54:31 +0100

    page details article

--- control/articles.php
+++ control/articles.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+        <title>Articles - OuterWilds Wiki</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
 <?php
 $articles = [
     [
@@ -63,30 +78,17 @@ if (isset($_GET['id'])) {
         exit;
     }
 } else {
-    echo "<div>tg.php</div>";
     ?>
-    <!DOCTYPE html>
-    <html lang="fr">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-        <title>Articles - OuterWilds Wiki</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
+    
         <?php
             $racine_path = '../';
             $titre = "Articles";
             include($racine_path."templates/front/header.php");
             include($racine_path."templates/front/article_card.php");
         ?>
-    </body>
-    </html>
     <?php
 }
 ?>
+
+</body>
+</html>
