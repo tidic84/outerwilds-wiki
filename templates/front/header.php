2ad5f16b2d47ccc2cbfe4397ed3370fd0d5451a1
tidic <tidic84@gmail.com>
2025-03-11 14:26:24 +0100

    ff

--- templates/front/header.php
+++ templates/front/header.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <?php if ($titre == "Accueil") echo "<link rel='stylesheet' href='$racine_path"."templates/front/css/style.css'"; ?>
 
     <title>OuterWilds Wiki</title>
 </head>
